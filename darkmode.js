f9b99431795e668cc5b40ff3f1e91a882b22a3cb
Sanket Fulzele <98740385+SanketFulzele@users.noreply.github.com>
2022-02-03 12:44:38 +0530

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,60 @@
+const body = document.querySelector('body'),
+toggleBtn = document.getElementById('btn');
+
+toggleBtn.addEventListener('click', () =>{
+    body.classList.toggle('active');
+    toggleBtn.classList.toggle('active');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
